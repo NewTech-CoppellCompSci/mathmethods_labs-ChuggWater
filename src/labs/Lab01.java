fcd3c7b5d63c77bddd2e46d7f79fad6ddfc61a9e
ChuggWater <jmccray@newtech.coppellisd.com>
2022-03-11 11:56:07 -0600

    Update Lab01.java

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,10 +1,12 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
 	public static void main(String[] args) {
+		
+		
 		
 		problem01();
 		problem02();
@@ -16,7 +18,18 @@ public class Lab01 {
 
 	
 	public static void problem01() {
+  
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.println("enter a positive integer");  
+		int input = inKey.nextInt();
+		
+		
+		System.out.println("enter another positive integer");  
+		int input2 = inKey.nextInt();
+		
+		double num1Pow = Math.pow(input,input2);
+		System.out.println(num1Pow + "\n");
 		
 		
 	}
@@ -26,7 +39,7 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
-		
+		System.out.println("enter a positive integer"); 
 		
 	}
 
@@ -35,17 +48,21 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.println("\n enter the first side of the triangle"); 
+		int input3 = inKey.nextInt();
 		
-
+		System.out.println("enter the second side of the triangle"); 
+		int input4 = inKey.nextInt();
+		
+		System.out.println();
 	}
 
 
 	
 	
 	public static void problem04() {
-		
-		
 		
 	}
 	
